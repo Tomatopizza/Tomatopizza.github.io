5c14c4bc1f4ee6744ca625d00ec8eaebe77285e9
na <lmh9205@gmail.com>
2023-06-15 13:47:10 +0900

    :sparkles: article_detail page

--- assets/js/article_detail.js
+++ assets/js/article_detail.js
@@ -7,4 +7,22 @@ window.onload = async function() {
 
   const response = await getArticle(articleId)
   console.log(response)
+
+  const articleTitle = document.getElementById("article_title")
+  const articleContent = document.getElementById("article_content")
+  const articleImage = document.getElementById("article_image")
+
+  articleTitle.innerText = response.title
+  articleContent.innerText = response.content
+  const newImage = document.createElement("img")
+  console.log(`${response.image}`)
+
+  if(response.image) {
+  newImage.setAttribute("src", `${backend_base_url}${response.image}`)
+  } else {
+    newImage.setAttribute("src", "https://health.clevelandclinic.org/wp-content/uploads/sites/3/2022/04/exerciseHowOften-944015592-770x533-1-650x428.jpg")
+  }
+  articleImage.appendChild(newImage)
+  
+
 }
